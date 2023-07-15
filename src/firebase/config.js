4e0ad730359b948ce2e9f32d0177a016af35efbd
usuario <jqncaraballo@gmail.com>
2023-07-14 23:14:21 -0300

    agregué firebase, pero solamente lo instalé y pegué en el config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5sk9bd3GmFYBaLzdjp6Tl5MDJob23vbg",
+  authDomain: "proyectofinalrjs.firebaseapp.com",
+  projectId: "proyectofinalrjs",
+  storageBucket: "proyectofinalrjs.appspot.com",
+  messagingSenderId: "180187417143",
+  appId: "1:180187417143:web:56a5c4cfda3620d7818db2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
